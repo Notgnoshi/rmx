171198ccbe895ea7c4825e700560a8a697f06041
Austin Gill <Notgnoshi@gmail.com>
2026-05-24 10:23:10 -0500

    cmake: Search for Catch2 from the environment before fetching it

--- cmake/FindCatch2.cmake
+++ cmake/FindCatch2.cmake
@@ -1,9 +1,13 @@
-include(FetchContent)
-
-FetchContent_Declare(
-    Catch2
-    GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-    GIT_TAG v3.7.0
-)
-
-FetchContent_MakeAvailable(Catch2)
+# Search the environment, and then fall back to FetchContent
+find_package(Catch2 3 CONFIG)
+if(NOT Catch2_FOUND)
+    include(FetchContent)
+    FetchContent_Declare(
+        Catch2
+        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+        GIT_TAG v3.15.0
+    )
+    FetchContent_MakeAvailable(Catch2)
+    list(APPEND CMAKE_MODULE_PATH ${catch2_SOURCE_DIR}/extras)
+    set(Catch2_FOUND TRUE)
+endif()
